8fe494a48088e0b521d3203cba681ca5656ce985
Xinyu Zeng <xzeng@cs.wisc.edu>
2022-08-14 16:27:00 +0800

    bj machine

--- cpp/src/parquet/file_reader.cc
+++ cpp/src/parquet/file_reader.cc
@@ -305,8 +305,10 @@ class SerializedFile : public ParquetFileReader::Contents {
                  const std::vector<int>& column_indices,
                  const ::arrow::io::IOContext& ctx,
                  const ::arrow::io::CacheOptions& options) {
-    cached_source_ =
-        std::make_shared<::arrow::io::internal::ReadRangeCache>(source_, ctx, options);
+    if (cached_source_ == nullptr) {
+      cached_source_ =
+          std::make_shared<::arrow::io::internal::ReadRangeCache>(source_, ctx, options);
+    }
     std::vector<::arrow::io::ReadRange> ranges;
     for (int row : row_groups) {
       for (int col : column_indices) {
