bfcc120ba8c240aa1c54bb2a82a3fa00ac9b10a5
Xinyu Zeng <xzeng@cs.wisc.edu>
2022-08-14 23:36:55 +0800

    update async reader

--- cpp/src/openformat/raw_reader_async.cc
+++ cpp/src/openformat/raw_reader_async.cc
@@ -18,6 +18,7 @@
 #include "arrow/dataset/file_base.h"
 #include "arrow/filesystem/api.h"
 #include "arrow/io/file.h"
+#include "arrow/util/checked_cast.h"
 #include "json.hpp"
 #include "parquet/arrow/writer.h"
 #include "parquet/file_reader.h"
@@ -34,7 +35,7 @@ arrow::Status async_read_file(parquet::ParquetFileReader* reader) {
   for (int i = 0; i < reader->metadata()->num_row_groups(); i++) {
     for (int j = 0; j < reader->metadata()->num_columns(); j++) {
       reader->PreBuffer({i}, {j}, io_context, cache_options);
-      reader->WhenBuffered({i}, {j}).Wait();
+      // reader->WhenBuffered({i}, {j}).Wait();
     }
   }
   return arrow::Status::OK();
@@ -58,7 +59,14 @@ arrow::Status RunMain(int argc, char** argv) {
   ARROW_UNUSED(use_threads);
   if (profiler_enabled) ProfilerStart(prof_name.c_str());
   // fs::S3FileSystem::
-  ARROW_ASSIGN_OR_RAISE(auto fs, fs::FileSystemFromUri(file_path));
+
+  ARROW_CHECK_OK(fs::InitializeS3(fs::S3GlobalOptions()));
+  auto s3_options = fs::S3Options::Defaults();
+  s3_options.region = "cn-north-1";
+  ARROW_ASSIGN_OR_RAISE(auto fs, fs::S3FileSystem::Make(s3_options));
+  // ARROW_ASSIGN_OR_RAISE(auto fs, fs::FileSystemFromUri(file_path));
+  // auto s3fs = arrow::internal::checked_pointer_cast<fs::S3FileSystem>(fs);
+  // s3fs->region
   ARROW_ASSIGN_OR_RAISE(auto input, fs->OpenInputFile(file_path));
   // fs::S3FileSystem::Make()
 
